cf1f143b7bd37c43da9aec537dc8b71532fd6c3a
Simply <leonardosidotiabate@gmail.com>
2026-01-03 23:47:04 +0100

    Add files via upload

---
+++ mvvm-template/MVVM_Template/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVM_Template.Core;
+
+namespace MVVM_Template.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand SecondViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public SecondViewModel SecondVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            SecondVM = new SecondViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => 
+            { 
+                CurrentView = HomeVM;
+            });
+
+            SecondViewCommand = new RelayCommand(o => 
+            { 
+                CurrentView = SecondVM;
+            });
+        }
+    }
+}
